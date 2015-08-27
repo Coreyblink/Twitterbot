5e4c00883b74a69a75efbe6bd9b1f5e11a80ff1b
Corey Harris <corey@mobilewebie.com>
2015-08-27 05:14:49 +0000

    deleted api keys

--- test/twitter.rb
+++ test/twitter.rb
@@ -3,10 +3,10 @@
 require 'twitter'
 
 CLIENT = Twitter::REST::Client.new do |config| 
-    config.consumer_key = ENV['wWEiCmgcSKmPjK7NVs5NwHhD9'] 
-    config.consumer_secret = ENV['bMckUoZm64PQlhSX7HiqAUYhzGXOd2vsAU6m2JSgxMOkpT5X9P'] 
-    config.access_token = ENV['258603558-BGtgKBbYR4cxbbf4D58jornKPn6spDynpP3mZnRq'] 
-    config.access_token_secret = ENV['eJ6z9G1gievqCdkDiNrnqfHRrwYzNCmLhDHAtCp7nVMvM'] 
+    config.consumer_key = ENV['#'] 
+    config.consumer_secret = ENV['#'] 
+    config.access_token = ENV['#'] 
+    config.access_token_secret = ENV['#'] 
 end
 
 client.update("I'm the man!")
